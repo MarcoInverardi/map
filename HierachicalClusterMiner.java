5c50cf79265c1bd53e4c890c147ccdc5f62ef519
Marco Inverardi <invy98@gmail.com>
2024-04-25 12:56:52 +0200

    modificato HierachicalClusterMiner

--- HierachicalClusterMiner.java
+++ HierachicalClusterMiner.java
@@ -1,5 +1,3 @@
-package lab1;
-
 class HierachicalClusterMiner {
 	
 	private Dendrogram dendrogram;
@@ -21,10 +19,30 @@ class HierachicalClusterMiner {
 		return dendrogram.toString(data);
 	}
 
+	void mine(Data data, ClusterDistance distance ){
+		void mine(Data data, ClusterDistance distance) {
+			int n = data.getNumberOfExamples();
+			ClusterSet clusterSet = new ClusterSet(n);
 
-	void mine(Data data, ClusterDistance distance) {
+			// Inizializza il ClusterSet con cui popolare il dendrogramma
+			for (int i = 0; i < n; i++) {
+				Cluster cluster = new Cluster();
+				cluster.addData(i);
+				clusterSet.add(cluster);
+			}
 
+			//inizializziamo la base del dendrogramma con tutti i cluster
+			int level = 0;
+			dendrogram.setClusterSet(clusterSet, level);
 
-
+			//effettuiamo i merge dei cluster livello per livello fino a raggiungere la radice
+			while (clusterSet.getSize() > 1) {
+				ClusterSet newClusterSet = clusterSet.mergeClosestClusters(distance, data);
+				level++;
+				dendrogram.setClusterSet(newClusterSet, level);
+				clusterSet = newClusterSet;
+			}
+		}
 	}
+	
 }
